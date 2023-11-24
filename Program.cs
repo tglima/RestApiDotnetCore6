703b45f1157187670c46c6a85308eedc377fb6d1
tglima <thg.limadesousa@gmail.com>
2023-11-24 14:53:12 -0300

    feat: Adiciona o service DbSQLiteContext - Garante a criação do banco de dados e das tabelas ao iniciar

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using WebApi.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
+var env = builder.Environment;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<DbSQLiteContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +19,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// ensure database and tables exist
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<DbSQLiteContext>();
+    await context.Init();
 }
 
 app.UseHttpsRedirection();
