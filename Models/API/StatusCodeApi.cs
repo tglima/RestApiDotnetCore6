9685ddd6c54c65dc681dcb47a6987bc25d496de1
tglima <thg.limadesousa@gmail.com>
2023-11-24 20:57:56 -0300

    feat: Implemeta o Enum StatusCodeApi

---
+++ Models/API/StatusCodeApi.cs
@@ -0,0 +1,11 @@
+namespace WebApi.Models.API
+{
+    public enum StatusCodeApi
+    {
+        SUCCESS = 200,
+        FAIL = 400,
+        UNNAUTHORIZED = 401,
+        NOT_FOUND = 404,
+        ERROR = 500
+    }
+}
