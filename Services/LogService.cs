e47c614410ec3595f12b1fdd25c3af8570793dec
tglima <thg.limadesousa@gmail.com>
2023-11-29 16:16:46 -0300

    feat: Adiciona novos métodos - SaveLogError: Será usado para persistir o log no banco de dados SQlite - SetRequestData: Responsável por obter as informações recebidas no request - SetResponseData: Responsável por obter as informações que serão retornadas no request

--- Services/LogService.cs
+++ Services/LogService.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using WebApi.Helpers;
+using System.Text.Json;
 using WebApi.Models.DTO;
 
 namespace WebApi.Services
@@ -9,6 +12,59 @@ namespace WebApi.Services
         public LogService()
         {
             this.LogDTO = new LogDTO();
+        }
+
+
+        public void SaveLogError(LogErrorDTO logError)
+        {
+            logError.CodeEvent = this.LogDTO.CodeEvent;
+        }
+
+        public void SaveLog()
+        {
+            this.LogDTO.DtFinish = AppHelper.GetDateNow();
+
+        }
+
+        public async Task SetRequestData(HttpContext context)
+        {
+            var headers = context.Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString());
+            var method = context.Request.Method;
+            var query = context.Request.Query.ToDictionary(q => q.Key, q => q.Value.ToString());
+            var url = string.Concat(context.Request.Path, context.Request.QueryString);
+
+            using var reader = new StreamReader(context.Request.Body, Encoding.UTF8);
+            var body = await reader.ReadToEndAsync();
+
+            var requestData = new
+            {
+                headers,
+                body,
+                method,
+                query,
+                url
+            };
+
+            this.LogDTO.RequestData = JsonSerializer.Serialize(requestData);
+            this.LogDTO.ApiKey = context.Request.Headers[Constant.API_KEY].ToString() ?? string.Empty;
+        }
+
+        public void SetResponseData(HttpResponse response, string body)
+        {
+            var headers = response.Headers.ToDictionary(h => h.Key, h => h.Value.ToString());
+            var statusCode = response.StatusCode;
+            var contentType = response.ContentType ?? string.Empty;
+
+            var responseData = new
+            {
+                headers,
+                statusCode,
+                contentType,
+                body,
+            };
+
+            this.LogDTO.ResponseData = JsonSerializer.Serialize(responseData);
+
         }
 
     }
