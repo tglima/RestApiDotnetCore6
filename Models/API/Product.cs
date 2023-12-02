fcc06f1244b7bbff1af816d86caded8a1be89bea
tglima <thg.limadesousa@gmail.com>
2023-12-02 19:19:18 -0300

    feat: Adiciona a class Product

---
+++ Models/API/Product.cs
@@ -0,0 +1,68 @@
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace WebApi.API
+{
+    [DisplayName("Product")]
+    public class Product
+    {
+        [JsonPropertyName("id_product")]
+        public int IdProduct { get; set; }
+
+        [JsonPropertyName("nm_product")]
+        public string NmProduct { get; set; } = string.Empty;
+
+        [JsonPropertyName("vl_month_price")]
+        public double VlMonthPrice { get; set; }
+
+        [JsonPropertyName("nm_videoQuality")]
+        public string NmVideoQuality { get; set; } = string.Empty;
+
+        [JsonPropertyName("nm_resolution")]
+        public string NmResolution { get; set; } = string.Empty;
+
+        [JsonPropertyName("qt_simultaneous_screens")]
+        public int QtSimultaneousScreens { get; set; }
+    }
+
+
+    [DisplayName("ArrayProduct")]
+    public class ArrayProduct
+    {
+        public ArrayProduct()
+        {
+            this.Products = new List<Product>();
+        }
+
+        public List<Product> Products { get; set; }
+    }
+
+
+    public class ProductExample
+    {
+        public Product GetExamples()
+        {
+            return new Product()
+            {
+                IdProduct = 1,
+                NmProduct = "Basic",
+                VlMonthPrice = 14.99,
+                NmResolution = "Good",
+                QtSimultaneousScreens = 1
+            };
+        }
+    }
+
+    public class ArrayProductExample
+    {
+        public ArrayProduct GetExamples()
+        {
+            var arrayProduct = new ArrayProduct();
+            var productExample = new ProductExample();
+            arrayProduct.Products.Add(productExample.GetExamples());
+            arrayProduct.Products.Add(productExample.GetExamples());
+            arrayProduct.Products.Add(productExample.GetExamples());
+            return arrayProduct;
+        }
+    }
+}
