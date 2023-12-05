d3727fb78be59cc17af3f284a473307c32d37c27
tglima <thg.limadesousa@gmail.com>
2023-12-05 19:10:09 -0300

    tests: Elabora testes para HealthCheckController

---
+++ WebApi.Tests/Controllers/HealthCheckControllerTests.cs
@@ -0,0 +1,39 @@
+using Xunit;
+using System;
+using System.Linq;
+using WebApi.Helpers;
+using WebApi.Models.API;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApi.Tests.Controllers
+{
+    public class HealthCheckControllerTests
+    {
+        [Fact]
+        public void HealthCheck_ReturnsOkResult()
+        {
+            var healthCheckController = new HealthCheckController();
+
+            var result = healthCheckController.Check();
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void HealthCheck_ReturnsExpectedJson()
+        {
+
+            var healthCheckController = new HealthCheckController();
+
+            var result = healthCheckController.Check() as OkObjectResult;
+
+            Assert.NotNull(result);
+
+            var healthCheckResponse = Assert.IsType<HealthCheckResponse>(result.Value);
+            Assert.Equal(Constant.OK, healthCheckResponse.Status);
+        }
+    }
+}
